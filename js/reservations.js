e3ed9354ad83fb7c69daeab721ebf763520916ff
Sechma <marokac@seznam.cz>
2021-11-29 20:26:03 +0100

    authors

--- js/reservations.js
+++ js/reservations.js
@@ -1,3 +1,10 @@
+/*
+ Project:    ITU - Rezervační kalendář
+ University: Brno University of Technology, FIT
+ Date:       29.11. 2021
+ Author:  Filip Solich <xsolic00@stud.fit.vutbr.cz> 
+*/
+
 function createDay(date) {
 	if (data[date] === undefined)
 		data[date] = [];
